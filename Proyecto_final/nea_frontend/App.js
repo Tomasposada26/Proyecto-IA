9483f47641fbce872d842a14e24ac9ea0ec0de42
Tomasposada26 <tomasposada26@gmail.com>
2025-11-03 17:17:56 -0500

    add nea_frontend/App.js

---
+++ Proyecto_final/nea_frontend/App.js
@@ -0,0 +1,10 @@
+// This file has been moved to src/App.js
+// ...existing code from App.js...
+import React from "react";
+import Chat from "./Chat";
+
+function App() {
+  return <Chat />;
+}
+
+export default App;
